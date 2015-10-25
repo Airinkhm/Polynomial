10e697ba3de21408dcb0a64c35a55813f1cee5c5
Airinkhm <eyerinkhorsand@hotmail.com>
2015-10-25 08:01:05 +0330

    A Dum Change

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println();
 		System.out.println("Enter Polynomials:");
 		Scanner myScanner = new Scanner(System.in);
 		String inputString1 = myScanner.nextLine();
